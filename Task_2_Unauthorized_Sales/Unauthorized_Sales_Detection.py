4cb147036d68512d94ea587c0ad89afd3afe2fe1
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-10 13:57:52 +0100

    Update Unauthorized_Sales_Detection.py

--- Task_2_Unauthorized_Sales/Unauthorized_Sales_Detection.py
+++ Task_2_Unauthorized_Sales/Unauthorized_Sales_Detection.py
@@ -6,11 +6,28 @@ app = Flask(__name__)
 @app.route('/detect_unauthorized_sales', methods=['POST'])
 def detect_unauthorized_sales():
     data = request.get_json()
-    print(data)
-    return identify_unauthorized_sales(data)
 
-def identify_unauthorized_sales(data):
-    return jsonify({"message": "Hello, World!"})
+    productListings = data.get('productListings', [])
+    salesTransactions = data.get('salesTransactions', [])
+
+    unauthorized_sales = identify_unauthorized_sales(productListings, salesTransactions)
+    print(unauthorized_sales)
+
+    return identify_unauthorized_sales(productListings, salesTransactions)
+
+def identify_unauthorized_sales(productListings, salesTransactions):
+    unauthorized_sales = []
+
+    authorized_sellers = {listing['productID']: listing['authorizedSellerID'] for listing in productListings}
+
+    for transaction in salesTransactions:
+        product_id = transaction['productID']
+        seller_id = transaction['sellerID']
+
+        if product_id not in authorized_sellers or seller_id != authorized_sellers[product_id]:
+            unauthorized_sales.append(transaction)
+            
+    return unauthorized_sales
 
 if __name__ == '__main__':
     app.run(debug=True)
