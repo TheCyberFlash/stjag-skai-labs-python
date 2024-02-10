392346d42cba27c5b076338e57dc0e5eb20217bb
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-10 14:30:14 +0100

    Update Optimal_Interview_Scheduling.py

--- Task_3_Optimal_Interview_Scheduling/Optimal_Interview_Scheduling.py
+++ Task_3_Optimal_Interview_Scheduling/Optimal_Interview_Scheduling.py
@@ -5,32 +5,37 @@ app = Flask(__name__)
 
 @app.route('/interview_schedule', methods=['POST'])
 def calculate_max_interviews():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    start_times = data.get('start_times', [])
-    end_times = data.get('end_times', [])
+        start_times = data.get('start_times', [])
+        end_times = data.get('end_times', [])
 
-    max_interviews = calculate_interviews(start_times, end_times)
-    print(max_interviews)
+        max_interviews = calculate_interviews(start_times, end_times)
+        print(max_interviews)
 
-    return jsonify({"max_interviews": max_interviews})
-
-def calculate_interviews(start_times, end_times):
-
-    if not start_times or not end_times or len(start_times) != len(end_times):
-        raise ValueError("Invalid input")
+        return jsonify({"max_interviews": max_interviews})  
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
     
-    sorted_interviews = sorted(zip(start_times, end_times), key=lambda x: x[1])
-
-    max_interviews = 0
-    previous_end_time = float('-inf')
-
-    for start, end in sorted_interviews:
-        if start >= previous_end_time:
-            max_interviews += 1
-            previous_end_time = end
+def calculate_interviews(start_times, end_times):
+    try:
+        if not start_times or not end_times or len(start_times) != len(end_times):
+            raise ValueError("Invalid input")
         
-    return max_interviews
+        sorted_interviews = sorted(zip(start_times, end_times), key=lambda x: x[1])
+
+        max_interviews = 0
+        previous_end_time = float('-inf')
+
+        for start, end in sorted_interviews:
+            if start >= previous_end_time:
+                max_interviews += 1
+                previous_end_time = end
+            
+        return max_interviews
+    except Exception as e:
+        raise e
 
 if __name__ == '__main__':
     app.run(debug=True)
