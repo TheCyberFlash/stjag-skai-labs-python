10b8a40af33e9ab13c3210aed761ad993fdfb743
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-10 08:16:30 +0100

    Update app.js

--- Task_1_Display_Polygon/app.js
+++ Task_1_Display_Polygon/app.js
@@ -7,8 +7,33 @@ fetch("polygon.json")
       geometry: new ol.geom.Polygon(data.coordinates),
     });
 
+    //Create a new source and layer
+    const vectorSource = new ol.source.Vector({
+      features: [polygon],
+    });
+
+    const vectorLayer = new ol.layer.Vector({
+      source: vectorSource,
+    });
+
+    // Create a map
+    const map = new ol.Map({
+      target: "map",
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM(),
+        }),
+        vectorLayer,
+      ],
+      view: new ol.View({
+        center: ol.extent.getCenter(polygonFeature.getGeometry().getExtent()),
+        zoom: 4,
+      }),
+    });
+
     console.log(polygon);
-  });
+  })
+  .catch((error) => console.error("Error:", error));
 
 // Create a Polygon
 // Create a map
