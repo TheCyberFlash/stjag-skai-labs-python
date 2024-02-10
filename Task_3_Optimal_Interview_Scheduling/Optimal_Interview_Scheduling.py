179f6fc7176d3fc1334f20693bd0107dc01455b2
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-10 14:22:07 +0100

    Update Optimal_Interview_Scheduling.py

--- Task_3_Optimal_Interview_Scheduling/Optimal_Interview_Scheduling.py
+++ Task_3_Optimal_Interview_Scheduling/Optimal_Interview_Scheduling.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 @app.route('/interview_schedule', methods=['POST'])
 def calculate_max_interviews():
     data = request.get_json()
+
     start_times = data.get('start_times', [])
     end_times = data.get('end_times', [])
-    print(start_times)
-    print(end_times)
-    return jsonify({"message": "Hello, World!"})
+
+    max_interviews = calculate_interviews(start_times, end_times)
+    print(max_interviews)
+
+    return jsonify({"max_interviews": max_interviews})
+
+def calculate_interviews(start_times, end_times):
+    return 0
 
 if __name__ == '__main__':
     app.run(debug=True)
