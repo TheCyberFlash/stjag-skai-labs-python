ac494bb6ec1df48e7a6f212d1390cffb59272bfb
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-10 08:06:35 +0100

    Update app.js

--- Task_1_Display_Polygon/app.js
+++ Task_1_Display_Polygon/app.js
@@ -1,4 +1,15 @@
 // Fetch JSON polygon.json
+fetch("polygon.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Create a Polygon
+    const polygon = new ol.Feature({
+      geometry: new ol.geom.Polygon(data.coordinates),
+    });
+
+    console.log(polygon);
+  });
+
 // Create a Polygon
 // Create a map
 // Bon Appétit!
